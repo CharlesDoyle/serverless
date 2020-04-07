ac9b319f34f5e839305283374bcdf3d596fa41d1
Charles Doyle <lguniversity@ymail.com>
2020-04-07 01:23:44 -0700

    Added comments to 08-dynamodb-create app

--- 08-dynamodb-create/index.js
+++ 08-dynamodb-create/index.js
@@ -1,5 +1,5 @@
 'use strict'
-
+// This file 
 const AWS = require('aws-sdk')
 const uuid = require('uuid')
 
@@ -8,15 +8,27 @@ const groupsTable = process.env.GROUPS_TABLE
 
 exports.handler = async (event) => {
   console.log('Processing event: ', event)
-  const itemId = uuid.v4()
+  // create a new partition ID for the groups Table
+  // uuid is a 3rd-party generator of UUIDs
+  // A UUID is a 128-bit number used to identify info in computers.
+  // Since the number has 128 hexadecimal bits, one uuid is considered 
+  // a unique identifier for an object in any system where it is used. 
+  // v4() is a function which generates a 'version 4 UUID'
+  // which uses a random number. 
+  const itemId = uuid.v4() // generate a random unique ID
 
+  // parse the body of the request coming from Postman, because
+  // it is a POST request with a new Item to be posted to the db table.
   const parsedBody = JSON.parse(event.body)
-
+  // create a new Item for a DynamoDB table
+  // Each new Item must have a partition ID
+  // ...parsedBody is a way to include the whole body object 
+  // as it is written
   const newItem = {
     id: itemId,
     ...parsedBody
   }
-
+  // post the new Item in my groups table on DynamoDB
   await docClient.put({
     TableName: groups,
     Item: newItem
